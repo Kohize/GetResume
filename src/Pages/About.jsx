93aaef7fa35eee5bfca67e4a4db6bf77b2074433
Kohize <kohi881@gmail.com>
2025-06-12 20:47:39 +0300

    initial for about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import Hero from '../features/About/ui/Hero/Hero';
+import Mission from '../features/About/ui/Mission/Mission';
+import Story from '../features/About/ui/Story/Story';
+import Stand from '../features/About/ui/Stand/Stand';
+import Announce from '../features/About/ui/Announce/Announce';
 
 export default function About() {
-  return <div>About Page</div>;
+  return (
+    <div>
+      <Hero />
+      <Mission />
+      <Story />
+      <Stand />
+      <Announce />
+    </div>
+  );
 }
